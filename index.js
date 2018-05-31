6136042336d1d8f6fe60602b0916659bc32b4c70
Nate Eckerson <nate.eckerson@gmail.com>
2018-05-31 19:18:09 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+ return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  var li = document.querySelectorAll('ul.ranked-list li')
+  
+  for (var i = 0; i < li.length; i++) {
+    var innerNum = parseInt(li[i].innerHTML);
+    li[i].innerHTML = (innerNum + n).toString()
+  }
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
