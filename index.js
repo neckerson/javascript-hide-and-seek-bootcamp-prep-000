d6c891b22df3a117b81d8782c772e4a714a3e671
Nate Eckerson <nate.eckerson@gmail.com>
2018-05-31 18:59:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ function nestedTarget() {
  return document.querySelector('#nested .target')
 }
 
-
+function increaseRankBy(n) {
+  
+}
 
 function deepestChild() {
   return document.querySelector('#grand-node div div div div')
